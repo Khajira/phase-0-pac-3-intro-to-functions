b2c653c722e7204db0c1a74921e45a31258b901b
Benard <benardkhajira@gmail.com>
2024-03-22 19:24:20 +0300

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,59 @@
 // Follow along with the examples here
+function doNothing() {}
+function sayHello() {
+    console.log("Hello!");
+  }
+  sayHello();
+function sayHelloToGuadalupe() {
+    console.log("Hello, Guadalupe!")
+ }
+ sayHelloToGuadalupe()
+function sayHelloToLiz() {
+    console.log("Hello, Liz!")
+ }
+ sayHelloToLiz()
+function sayHelloToSamip() {
+    console.log("Hello, Samip!")
+ }
+ sayHelloToSamip()
+
+ sayHelloToGuadalupe()
+ sayHelloToLiz()
+ sayHelloToSamip()
+
+ function doSomething(thing) {
+    console.log(thing);
+  }
+doSomething("anything"); // passing the argument 'anything' into our function
+
+function sayHelloTo(firstName) {
+    console.log(`Hello, ${firstName}!`)
+ }
+
+ sayHelloTo("Guadalupe"); //"Hello, Guadalupe!"
+ sayHelloTo("Jane"); //"Hello, Jane!"
+ sayHelloTo("R2-D2"); //"Hello, R2-D2"
+ sayHelloTo("1"); //"Hello, 1!"
+
+ function say(greeting, firstName) {
+    console.log(`${greeting}, ${firstName}!`);
+  }
+  say("Goodbye", "Julio");
+  say("Julio", "hello");
+  function say(greeting, firstName) {
+    console.log("firstName: ", firstName);
+    console.log("greeting: ", greeting);
+    console.log(`${greeting}, ${firstName}!`);
+  }
+  function add(x, y) {
+    return x + y;
+  }
+  add(1, 2)
+  console.log(add(1, 2));
+  function say(greeting, firstName) {
+    return `${greeting}, ${firstName}!`;
+  }
+  function say(greeting, firstName) {
+    console.log(`${greeting}, ${firstName}!`);
+  }
+  console.log(say("Hello", "Liz"));
